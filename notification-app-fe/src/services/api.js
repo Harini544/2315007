e3368b029e987692870688dc8785e1b28704bad3
Harini544 <2315007@nec.edu.in>
2026-07-02 12:37:27 +0530

    Completed Stage 7 - Notification Dashboard

--- notification-app-fe/src/services/api.js
+++ notification-app-fe/src/services/api.js
@@ -1,8 +1,36 @@
-import axios from "axios";
-
-const API = "http://localhost:3000/notifications";
-
 export async function getNotifications() {
-  const response = await axios.get(API);
-  return response.data.notifications || [];
+
+    return [
+        {
+            ID: "1",
+            Type: "Placement",
+            Message: "Google Interview Scheduled",
+            Timestamp: "2026-04-22 17:51:18"
+        },
+        {
+            ID: "2",
+            Type: "Result",
+            Message: "Mid-Sem Result Published",
+            Timestamp: "2026-04-22 17:50:54"
+        },
+        {
+            ID: "3",
+            Type: "Event",
+            Message: "Tech Fest Tomorrow",
+            Timestamp: "2026-04-22 17:50:06"
+        },
+        {
+            ID: "4",
+            Type: "Placement",
+            Message: "Amazon Hiring",
+            Timestamp: "2026-04-22 17:49:42"
+        },
+        {
+            ID: "5",
+            Type: "Result",
+            Message: "Project Review",
+            Timestamp: "2026-04-22 17:49:54"
+        }
+    ];
+
 }
